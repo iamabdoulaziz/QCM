a392ae47535983a89cf3bbd870e66f39f579fb37
iamabdoulaziz <abdoulazizc867@gmail.com>
2024-11-17 00:26:53 +0000

    Simple QCM.

---
+++ main.py
@@ -0,0 +1,54 @@
+import time
+
+print("Quel est la capital de la France ?")
+print("a) Marseille")
+print("b) Nice")
+print("c) Paris")
+print("d) Nantes")
+
+answer = input("Ta réponse : ")
+if answer == "c":
+    print("Bonne réponse !")
+    time.sleep(1)
+else :
+    print("Mauvaises réponse !")
+    time.sleep(1)
+
+print("+++++++******+++++++++")
+print("Quel est la capital du Mali?")
+print("a) Bamako")
+print("b) Kidal")
+print("c) Tombouctou")
+print("d) Segou")
+answer = input("Ta réponse : ")
+if answer == "a":
+    print("Bonne réponse !")
+    time.sleep(1)
+else :
+    print("Mauvaises réponse !")
+    time.sleep(1)
+
+print("+++++++******+++++++++")
+print("Quel est la capital du Japon?")
+print("a) Bourem")
+print("b) Lagos")
+print("c) Sebenikoro")
+print("d) Tokyo")
+answer = input("Ta réponse : ")
+if answer == "d":
+    print("Bonne réponse !")
+    time.sleep(1)
+else :
+    print("Mauvaises réponse !")
+    time.sleep(1)
+print("+++++++******+++++++++")
+print("Quel est la capital de l'Italie?")
+print("a) Milan")
+print("b) Rome")
+print("c) Naples")
+print("d) Florence")
+answer = input("Ta réponse : ")
+if answer == "b":
+    print("Bonne réponse !")
+else :
+    print("Mauvaises réponse !")
